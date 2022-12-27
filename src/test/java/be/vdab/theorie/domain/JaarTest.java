5b59e62fe02357bf6113dc4929e53f69925e081d
Peter D'hondt <dhondt.peter@gmail.com>
2022-12-27 10:57:15 +0100

    added @DisplayName to JaarTest

--- src/test/java/be/vdab/theorie/domain/JaarTest.java
+++ src/test/java/be/vdab/theorie/domain/JaarTest.java
@@ -1,27 +1,33 @@
 package be.vdab.theorie.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class JaarTest {
     @Test
+    @DisplayName("Een jaar deelbaar door 400 is een schrikkeljaar.")
     void eenJaarDeelbaarDoor400IsEenSchrikkeljaar() {
         var jaar = new Jaar(2000);
         assertThat(jaar.isSchrikkeljaar()).isTrue();
     }
     @Test
+    @DisplayName("Een jaar deelbaar door 100 is geen schrikkeljaar.")
     void eenJaarDeelbaarDoor100IsGeenSchrikkeljaar() {
         assertThat(new Jaar(1900).isSchrikkeljaar()).isFalse();
     }
     @Test
+    @DisplayName("Een jaar deelbaar door 4 is een schrikkeljaar.")
     void eenJaarDeelbaarDoor4IsEenSchrikkeljaar() {
         assertThat(new Jaar(2012).isSchrikkeljaar()).isTrue();
     }
     @Test
+    @DisplayName("Een jaar niet deelbaar door 4 is geen schrikkeljaar.")
     void eenJaarNietDeelbaarDoor4IsGeenSchrikkeljaar() {
         assertThat(new Jaar(2015).isSchrikkeljaar()).isFalse();
     }
     @Test
+    @DisplayName("Method toString() geeft het jaar als tekst terug.")
     void toStringGeeftHetJaarAlsTekst() {
         assertThat(new Jaar(2022)).hasToString("2022");
     }
