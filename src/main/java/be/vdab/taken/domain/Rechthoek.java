facdda77ac5403c1d1e45d05b11cc7f3d88726cb
Peter D'hondt <dhondt.peter@gmail.com>
2022-12-28 11:49:28 +0100

    taak 1.6 equals en hashCode

--- src/main/java/be/vdab/taken/domain/Rechthoek.java
+++ src/main/java/be/vdab/taken/domain/Rechthoek.java
@@ -1,5 +1,7 @@
 package be.vdab.taken.domain;
 
+import java.util.Objects;
+
 public class Rechthoek {
     private final int lengte;
     private final int breedte;
@@ -15,5 +17,13 @@ public class Rechthoek {
 
     public int getOmtrek() {
         return 2 * (lengte + breedte);
+    }
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof Rechthoek r && lengte == r.lengte && breedte == r.breedte ;
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(lengte, breedte);
     }
 }
--- src/test/java/be/vdab/taken/domain/RechthoekTest.java
+++ src/test/java/be/vdab/taken/domain/RechthoekTest.java
@@ -1,16 +1,35 @@
 package be.vdab.taken.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RechthoekTest {
+    private Rechthoek rechthoek;
+    @BeforeEach
+    void beforeEach() {
+        rechthoek = new Rechthoek(5, 4);
+    }
     @Test
     void deOppervlakteVanEenRechthoekVan5Op4Is20() {
-        assertThat(new Rechthoek(5, 4).getOppervlakte()).isEqualTo(20);
+        assertThat(rechthoek.getOppervlakte()).isEqualTo(20);
     }
     @Test
     void deOmtrekVanEenRechthoekVan5Op4Is18() {
-        assertThat(new Rechthoek(5, 4).getOmtrek()).isEqualTo(18);
+        assertThat(rechthoek.getOmtrek()).isEqualTo(18);
+    }
+    @Test
+    void tweeRechthoekenZijnGelijkWanneerLengteEnBreedteGelijkZijn() {
+        assertThat(rechthoek).isEqualTo(new Rechthoek(5, 4));
+    }
+    @Test
+    void tweeRechthoekenZijnVerschillendWanneerLengteOfBreedteVerschillendZijn() {
+        assertThat(rechthoek).isNotEqualTo(new Rechthoek(5, 6));
+        assertThat(rechthoek).isNotEqualTo(new Rechthoek(6, 4));
+    }
+    @Test
+    void tweeRechthoekenMetDezelfdeAfmetingenGevenDezelfdeHashcode() {
+        assertThat(rechthoek).hasSameHashCodeAs(new Rechthoek(5, 4));
     }
 
 }
