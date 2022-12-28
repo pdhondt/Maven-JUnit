5fedf3484b230eda206fe3e3f9d0bc0755ec5b2a
Peter D'hondt <dhondt.peter@gmail.com>
2022-12-28 10:50:52 +0100

    taak 1.5 Test fixtures

--- src/test/java/be/vdab/taken/domain/VeilingTest.java
+++ src/test/java/be/vdab/taken/domain/VeilingTest.java
@@ -1,5 +1,6 @@
 package be.vdab.taken.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,17 +9,20 @@ import java.math.BigDecimal;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class VeilingTest {
+    private Veiling veiling;
+    @BeforeEach
+    void beforeEach() {
+        veiling = new Veiling();
+    }
     @Test
     @DisplayName("Wanneer er nog geen bod gedaan is, is het hoogste bod 0")
     void hoogsteBodIsGelijkAan0WanneerNogGeenBodGedaanIs() {
-        var veiling = new Veiling();
         assertThat(veiling.getHoogsteBod()).isZero();
     }
 
     @Test
     @DisplayName("Wanneer een bod het eerste bod is (hier 10€), is hoogste bod gelijk aan dit bod (10€)")
     void hoogsteBodIsGelijkAanBodWanneerBodHetEersteBodIs() {
-        var veiling = new Veiling();
         veiling.doeBod(BigDecimal.TEN);
         assertThat(veiling.getHoogsteBod()).isEqualByComparingTo("10");
     }
@@ -26,7 +30,6 @@ class VeilingTest {
     @Test
     @DisplayName("Wanneer je meerdere keren een bod doet (30, 50, 20), is het hoogste bod gelijk aan het hoogste bedrag (50)")
     void hoogsteBodIsGelijkAanHoogsteBedragWanneerJeMeerdereKerenEenBodDoet() {
-        var veiling = new Veiling();
         veiling.doeBod(BigDecimal.valueOf(30));
         veiling.doeBod(BigDecimal.valueOf(50));
         veiling.doeBod(BigDecimal.valueOf(20));
