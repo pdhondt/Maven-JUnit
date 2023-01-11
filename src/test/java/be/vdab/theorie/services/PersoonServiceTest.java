1d735005d6f2013f32c3b4fcf826da3e8d504c8d
Peter D'hondt <dhondt.peter@gmail.com>
2023-01-11 22:23:03 +0100

    hoofdstuk 16 Mock

--- src/test/java/be/vdab/theorie/services/PersoonServiceTest.java
+++ src/test/java/be/vdab/theorie/services/PersoonServiceTest.java
@@ -1,19 +1,36 @@
 package be.vdab.theorie.services;
 
-import be.vdab.theorie.repositories.PersoonRepositoryStub;
+import be.vdab.theorie.repositories.PersoonRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class PersoonServiceTest {
+    @Mock
+    private PersoonRepository repository;
     private PersoonService service;
     @BeforeEach
     void beforeEach() {
-        service = new PersoonService(new PersoonRepositoryStub());
+        service = new PersoonService(repository);
     }
     @Test
     void deWeddeStandaardAfwijkingIs2() {
+        when(repository.findAllWeddes()).thenReturn(
+                Stream.of(2, 4, 4, 4, 5, 5, 7, 9)
+                        .map(getal -> BigDecimal.valueOf(getal))
+                        .toList()
+        );
         assertThat(service.standaardAfwijkingWeddes()).isEqualByComparingTo("2");
+        verify(repository).findAllWeddes();
     }
 }
